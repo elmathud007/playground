073cfd5cde7a9df72ce48eca9f5176cf961b070a
elmathud007 <elmathud007@gmail.com>
2016-01-25 22:46:49 +0000

    broken s3 update

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,16 +3,8 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/,
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
-  :url  => ":s3_domain_url",
-    :path => "public/medium/:id/:style_:basename.:extension",
-    :storage => :fog,
-    :fog_credentials => {
-        provider: 'AWS',
-        aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-    },
-    fog_directory: ENV["FOG_DIRECTORY"]
+  
 end
  
